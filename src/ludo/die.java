309ddcb9609c6268024aa6514402f6ec31ab77d2
taweesak <topcpe@gmail.com>
2012-10-24 14:37:08 +0700

    die

---
+++ src/ludo/die.java
@@ -0,0 +1,23 @@
+
+package ludo;
+
+import java.util.Random;
+
+public class die {
+   
+ 
+    
+    public void die(){
+    
+      
+        
+    Random rn = new Random();
+   
+    int r ;
+    r = rn.nextInt(6)+1;
+    
+   
+}
+
+
+}
