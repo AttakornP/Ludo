d24faa27141ad77ad9cb999e606734d1a2abfb83
taweesak <topcpe@gmail.com>
2012-10-25 09:22:14 +0700

    update

--- src/ludo/die.java
+++ src/ludo/die.java
@@ -1,15 +1,21 @@
-
 package ludo;
 
 import java.util.Random;
 
+
 public class die {
+        
    
-   public int roll_die(){
+ 
     
-   Random rn = new Random();
-   int r = rn.nextInt(6)+1;
-   
-   return r;
-   } 
+    public int roll_die() {
+
+        Random rn = new Random();
+        int r = rn.nextInt(6) + 1;
+        
+        return r;
+        
+        
+    }   
 }
+
